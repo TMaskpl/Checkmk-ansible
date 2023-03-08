2cd0ef3f6b019b797851252fa88de8bd39a8b6e6
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2023-03-08 14:36:19 +0100

    Update checkmk_update_service.sh

--- checkmk_update_service.sh
+++ checkmk_update_service.sh
@@ -1,7 +1,19 @@
+##################################################
+#                                                #
+#__author__ = "biuro@tmask.pl"                   #
+#__copyright__ = "Copyright (C) 2023 TMask.pl"   #
+#__license__ = "MIT License"                     #
+#__version__ = "1.0"                             #
+#                                                #
+##################################################
+
+
+
 conn=`cat /etc/systemd/system/check_mk.socket | grep MaxConnectionsPerSource | awk -F '=' '{print $2}'`
-conn_p=$((conn+10))
+conn_p=$((conn+50))
 echo $conn_p
-sed -E "s/MaxConnectionsPerSource=(.*)/MaxConnectionsPerSource=$conn_p/" /etc/systemd/system/check_mk.socket
+sed -E -i "s/MaxConnectionsPerSource=(.*)/MaxConnectionsPerSource=$conn_p/" /etc/systemd/system/check_mk.socket
+cat /etc/systemd/system/check_mk.socket
 systemctl daemon-reload
 systemctl restart check_mk.socket
 systemctl status check_mk.socket
